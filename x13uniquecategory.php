e9d1ba31cd21081a50cca176bda3e91818a89ba4
patryk <patryk.nietyksza@gmail.com>
2023-06-03 16:18:40 +0200

    first commit

---
+++ x13uniquecategory.php
@@ -0,0 +1,68 @@
+<?php
+/**
+* 2007-2023 PrestaShop
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author    PrestaShop SA <contact@prestashop.com>
+*  @copyright 2007-2023 PrestaShop SA
+*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*/
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class X13uniquecategory extends Module
+{
+    protected $config_form = false;
+
+    public function __construct()
+    {
+        $this->name = 'x13uniquecategory';
+        $this->tab = 'administration';
+        $this->version = '1.1.0';
+        $this->author = 'Patryk Nietykszaa';
+        $this->need_instance = 0;
+
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('Unique category');
+        $this->description = $this->l('Module written for x13 recruting process. Module adds new column in category view, thanks to this logged user have got access to this unique category.');
+
+        $this->ps_versions_compliancy = ['min' => '1.7', 'max' => _PS_VERSION_];
+    }
+
+    public function install()
+    {
+        Configuration::updateValue('X13UNIQUECATEGORY_LIVE_MODE', false);
+
+        return parent::install() &&
+            $this->registerHook('header') &&
+            $this->registerHook('displayBackOfficeHeader');
+    }
+
+    public function uninstall()
+    {
+        Configuration::deleteByName('X13UNIQUECATEGORY_LIVE_MODE');
+
+        return parent::uninstall();
+    }
+}
